a57664dc2f16c59c6d3fd67cc282c14e75e2fcc3
ish <tmdhgks2515@idrsystems.co.kr>
2025-01-09 19:48:39 +0900

    .

--- src/test/kotlin/WebsocketSimulation.kt
+++ src/test/kotlin/WebsocketSimulation.kt
@@ -1,46 +1,60 @@
 import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.core.CoreDsl.*
 import io.gatling.javaapi.http.HttpDsl.*
+import java.util.concurrent.atomic.AtomicInteger
 
 class WebsocketSimulation : Simulation() {
 
     private val httpProtocol = http
             .wsBaseUrl("ws://localhost:8080")
 
-    private var senderId = 1
+/*    private val messageCount = AtomicInteger(0)
 
-    // 구독자 시나리오
     private val subscriberScenario = scenario("Chat Room Subscriber Test")
             .exec(
-                    ws("Connect to Chat Room").connect("/chat/gatling").await(60).on(
-                            ws.checkTextMessage("Message Received")
-                                    .check(jsonPath("$.content").exists())
-                    ),
+                    ws("Connect to Chat Room")
+                            .connect("/chat/gatling")
+//                            .await(60).on(
+//                                    ws.checkTextMessage("Message Received")
+//                                            .check(jsonPath("$.content").`is`("Hello, I'm sender"))
+//                            )
             )
-            .pause(1)
+            .pause(180)
+            .exec(
+                    // collect the last text message and store it in the Session
+                    ws.processUnmatchedMessages { messages, session ->
+
+                        check(messages.count().equals(messageCount.get()))
+                    }
+            )
             .exec(
                     ws("Close Connection").close()
-            )
+            )*/
 
-    // 발신자 시나리오
-    private val publisherScenario = scenario("Message Publisher Test")
-            .pause(30)
+    private val scenario = scenario("WebSocket Test")
             .exec(
-                    ws("Connect as Publisher").connect("/chat/gatling")
+                    ws("Connect to Channel").connect("/chat/test-2").onConnected(
+                            repeat(2, "i").on(
+                                    exec(
+                                            ws("Send Broadcast Message")
+                                                    .sendText("Hello")
+                                                    .await(3).on(
+                                                            ws.checkTextMessage("Message Received")
+                                                                    .check(jsonPath("$.content").`is`("Hello"))
+                                                    )
+                                    ).pause(1)
+                            )
+                    )
             )
-            .exec(
-                    ws("Send Broadcast Message")
-                            .sendText("Hello, I'm ${senderId++}")
-            )
-            .pause(1)
             .exec(
                     ws("Close Publisher").close()
             )
 
     init {
         setUp(
-                subscriberScenario.injectOpen(rampUsers(10000).during(30)),
-                publisherScenario.injectOpen(rampUsers(1000).during(5)),
+                scenario.injectOpen(
+                        rampUsers(10000).during(20),
+                )
         ).protocols(httpProtocol)
     }
 }
