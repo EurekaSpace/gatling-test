113c030d59661711d0325b6d733967622d39c583
ish <tmdhgks2515@idrsystems.co.kr>
2025-01-24 19:47:39 +0900

    websocket 테스트 시나리오 수정

--- src/test/kotlin/WebsocketSimulation.kt
+++ src/test/kotlin/WebsocketSimulation.kt
@@ -2,47 +2,21 @@ import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.core.CoreDsl.*
 import io.gatling.javaapi.http.HttpDsl.*
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.Duration.Companion.seconds
 
 class WebsocketSimulation : Simulation() {
 
     private val httpProtocol = http
-            .wsBaseUrl("ws://localhost:8080")
-
-/*    private val messageCount = AtomicInteger(0)
-
-    private val subscriberScenario = scenario("Chat Room Subscriber Test")
-            .exec(
-                    ws("Connect to Chat Room")
-                            .connect("/chat/gatling")
-//                            .await(60).on(
-//                                    ws.checkTextMessage("Message Received")
-//                                            .check(jsonPath("$.content").`is`("Hello, I'm sender"))
-//                            )
-            )
-            .pause(180)
-            .exec(
-                    // collect the last text message and store it in the Session
-                    ws.processUnmatchedMessages { messages, session ->
-
-                        check(messages.count().equals(messageCount.get()))
-                    }
-            )
-            .exec(
-                    ws("Close Connection").close()
-            )*/
+            .wsBaseUrl("ws://localhost:8081")
 
     private val scenario = scenario("WebSocket Test")
             .exec(
-                    ws("Connect to Channel").connect("/chat/test-2").onConnected(
-                            repeat(2, "i").on(
+                    ws("Connect to Channel").connect("/chat/ch1").onConnected(
+                            repeat(3, "i").on(
                                     exec(
                                             ws("Send Broadcast Message")
                                                     .sendText("Hello")
-                                                    .await(3).on(
-                                                            ws.checkTextMessage("Message Received")
-                                                                    .check(jsonPath("$.content").`is`("Hello"))
-                                                    )
-                                    ).pause(1)
+                                    ).pause(10)
                             )
                     )
             )
@@ -53,7 +27,8 @@ class WebsocketSimulation : Simulation() {
     init {
         setUp(
                 scenario.injectOpen(
-                        rampUsers(10000).during(20),
+                        rampUsers(1000).during(30), // 30초 동안 1000명 점진적 연결
+//                        constantUsersPerSec(100.0).during(120) // 2분 동안 초당 100명 연결
                 )
         ).protocols(httpProtocol)
     }
